a662537bf50620b5285aec553615cb767be915ac
dennomwas <githinji.mwangi@gmail.com>
2017-05-07 14:24:43 +0300

    [Feature] Add user registration

--- bucket_api/auth/views.py
+++ bucket_api/auth/views.py
@@ -11,11 +11,41 @@ auth_api = Api(auth_blueprint)
 
 
 
+class RegisterResource(Resource):
+    def post(self):
+
+        new_user = request.get_json()
+
+        if not new_user:
+            return {'error': 'No input provided'}, 400
+
+        validate_user_errors = user_schema.validate(new_user)
+        if validate_user_errors:
+            return {'error': 'Check your fields and try again!'}, 400
+
+        username = new_user.get('username')
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            return {'error': ' username already exists!'}, 409
+
+        try:
+            user = User(fullname=new_user['fullname'],
+                        username=new_user['username'],
+                        password=new_user['password'])
+
+            db.session.add(user)
+            db.session.commit()
+
+            return {'message': username + ' successfully registered'}, 201
+
+        except Exception as error:
+
+            db.session.rollback()
+            return {'error': username + ' not registered, check your details and try again!'}
+
+
 
 auth_api.add_resource(RegisterResource, '/register/')
 auth_api.add_resource(RegisteredUsersResource, '/users')
 auth_api.add_resource(LoginResource, '/login/')
-
-
-
-
