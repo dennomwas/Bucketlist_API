a64d61c5054934e6c7750089fc8bf3fba0bd3d18
dennomwas <githinji.mwangi@gmail.com>
2017-05-07 15:32:52 +0300

    [Feature] Refactor imports

--- bucket_api/auth/views.py
+++ bucket_api/auth/views.py
@@ -3,7 +3,7 @@ from flask_restful import Api, Resource
 from sqlalchemy.exc import SQLAlchemyError
 
 
-from models import db, auth, tokenization, User, UserSchema
+from bucket_api.models import db, auth, tokenization, User, UserSchema
 
 auth_blueprint = Blueprint('auth_blueprint', __name__)
 user_schema = UserSchema()
