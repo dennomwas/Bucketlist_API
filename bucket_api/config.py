1083c13e032c0aa46f4fbc69638c9f98230785f1
dennomwas <githinji.mwangi@gmail.com>
2017-05-07 13:37:56 +0300

    [Feature] Add configurations file

---
+++ bucket_api/config.py
@@ -0,0 +1,53 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    """ common configurations"""
+    PORT = 5000
+    HOST = "127.0.0.1"
+    SECRET_KEY = "something_not_easy"
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}: {DB_PASS}@{DB_ADDR}/{DB_NAME}"\
+        .format(DB_USER="mwas", DB_PASS="", DB_ADDR="127.0.0.1", DB_NAME="bucketlist_db")
+
+
+
+class DevelopmentConfig(Config):
+    """ Development configurations"""
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+    PAGINATION_PAGE_SIZE = 5
+    PAGINATION_PAGE_ARGUMENT_NAME = 'page'
+
+
+class ProductionConfig(Config):
+    """ Production Configurations"""
+
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    """ Testing configurations"""
+
+
+app_config = {
+
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig,
+    'default': DevelopmentConfig
+}
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# DEBUG = True
+# PORT = 5000
+# HOST = "127.0.0.1"
+# SQLALCHEMY_ECHO = False
+# SQLALCHEMY_TRACK_MODIFICATIONS = True
+# SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}: {DB_PASS}@{DB_ADDR}/{DB_NAME}"\
+#     .format(DB_USER="mwas", DB_PASS="", DB_ADDR="127.0.0.1", DB_NAME="bucketlist_db")
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
