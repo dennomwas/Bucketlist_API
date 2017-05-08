6c3eaaa5d0743ee64d56e99f9b8e211d2b60e6b1
dennomwas <githinji.mwangi@gmail.com>
2017-05-08 08:11:53 +0300

    [Chore] Change folder level

---
+++ run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+
+from bucket_api import db, create_app
+
+
+app = create_app('development')
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+# def make_shell_context():
+#     return dict(app=app, db=db, User=User)
+# manager.add_command("shell", Shell(make_context=make_shell_context))
+
+if __name__ == '__main__':
+    manager.run()
